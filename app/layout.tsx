c123fc902921caeeed4cfb7c353fe85ffc2a519c
developMEnew0X <165547551+developMEnew0X@users.noreply.github.com>
2024-12-25 18:01:42 +0530

    first commitdgkhggytm

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} min-h-screen`}>
+      <body className={`${inter.className} max-h-screen`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
