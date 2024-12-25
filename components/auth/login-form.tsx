fc2f08a54f9004f372972c0e72733b690ea28701
developMEnew0X <165547551+developMEnew0X@users.noreply.github.com>
2024-12-25 14:32:42 +0530

    first commitd

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { GoogleAuthButton } from "./google-auth-button";
-import { Separator } from "@/components/ui/separator";
+// import { Separator } from "@/components/ui/separator";
 
 const formSchema = z.object({
   email: z.string().email(),
