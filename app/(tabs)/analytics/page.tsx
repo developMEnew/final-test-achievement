35666ec88b4d19dd22f5e77f580778f60dce5ff1
developMEnew0X <165547551+developMEnew0X@users.noreply.github.com>
2024-12-25 18:04:58 +0530

    first commitdgkhggyyhy

--- app/(tabs)/analytics/page.tsx
+++ app/(tabs)/analytics/page.tsx
@@ -2,7 +2,7 @@ import { ViewTab } from "@/components/v0/view-tab";
 
 export default function AnalyticsPage() {
   return (
-    <div className="pt-10">
+    <div className="pt-10 min-h-screen">
       <ViewTab />
     </div>
   );
